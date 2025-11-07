8e3354d143d7e2f0fd58266901484347dde80f77
tav <wcaka78@gmail.com>
2025-11-07 16:09:04 +0800

    商城首頁api

--- src/routes/api-mall.ts
+++ src/routes/api-mall.ts
@@ -33,7 +33,7 @@ router.get("/keyword", async (req: Request, res: Response) => {
 //推薦商品路由
 router.get("/recommend", async (req: Request, res: Response<ApiResponse>) => {
   const data = await prisma.product.findMany({
-    take: 12,
+    take: 24,
     skip: 30,
     include: {
       ProductVariants: true,
@@ -49,13 +49,90 @@ router.get("/recommend", async (req: Request, res: Response<ApiResponse>) => {
   res.json(response);
 });
 
-//購物車
+//新上架
+router.get("/new", async (req: Request, res: Response<ApiResponse>) => {
+  const data = await prisma.product.findMany({
+    take: 24,
+    orderBy: {
+      id: "desc",
+    },
+    include: {
+      ProductVariants: true,
+      ProductPics: true,
+    },
+  });
+
+  const response: ApiResponse = {
+    success: true,
+    data: data,
+  };
+
+  res.json(response);
+});
+
+//熱門
+router.get("/hot", async (req: Request, res: Response<ApiResponse>) => {
+  const allProducts = await prisma.product.findMany({
+    include: {
+      ProductVariants: true,
+      ProductPics: true,
+    },
+  });
+
+  const sortedProducts = allProducts
+    .map((product) => {
+      const minStock = product.ProductVariants.reduce((min, variant) => {
+        return Math.min(min, variant.stock);
+      }, Infinity);
+
+      return {
+        ...product,
+        _minStock: minStock,
+      };
+    })
+    .sort((a, b) => a._minStock - b._minStock)
+    .slice(0, 24);
+
+  const response: ApiResponse = {
+    success: true,
+    data: sortedProducts,
+  };
+
+  res.json(response);
+});
+
+//拿取特定商品資料
 router.get(
-  "/cart",
-  jwtParseMiddleware,
-  requireAuth,
-  async (req: Request, res: Response) => {
-    res.json("ok");
+  "/product/:product_id",
+  async (req: Request, res: Response<ApiResponse>) => {
+    const id = parseInt(req.params.product_id);
+    if (!id) {
+      const response: ApiErrorResponse = {
+        success: false,
+        error: "找不到該產品",
+      };
+      res.status(401).json(response);
+    }
+    const data = await prisma.product.findUnique({
+      where: { id: id },
+      include: {
+        ProductVariants: true,
+        ProductPics: true,
+      },
+    });
+
+    if (!data) {
+      const response: ApiErrorResponse = {
+        success: false,
+        error: "找不到該產品",
+      };
+      res.status(401).json(response);
+    }
+    const response: ApiResponse = {
+      success: true,
+      data: data,
+    };
+    res.json(response);
   }
 );
 
