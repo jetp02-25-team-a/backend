693d7a7f3cbf189f07851f628ca165750d7a7fe6
tav <wcaka78@gmail.com>
2025-11-11 11:04:54 +0800

    ecpay

--- src/routes/api-mall.ts
+++ src/routes/api-mall.ts
@@ -1,11 +1,21 @@
-import express from "express";
+import express, { response } from "express";
 import type { Request, Response } from "express";
 import { prisma } from "../utils/prisma-pagination.js";
 import { jwtParseMiddleware, requireAuth } from "../middleware/jwt.js";
+import { Merchant, CreditOneTimePayment } from "node-ecpay-aio";
+import "dotenv/config";
 
 import type { ApiResponse, ApiErrorResponse } from "../interfaces/index.js";
 
 const router = express.Router();
+
+const merchant = new Merchant("Test", {
+  MerchantID: process.env.MERCHANT_ID || "",
+  HashKey: process.env.ECPAY_HASHKEY || "",
+  HashIV: process.env.ECPAY_HASHIV || "",
+  ReturnURL: "https://declaratory-abdiel-numbingly.ngrok-free.dev/api/order",
+  ClientBackURL: "http://localhost:3000/shops",
+});
 
 //關鍵字搜尋
 router.get("/keyword", async (req: Request, res: Response) => {
@@ -135,5 +145,80 @@ router.get(
     res.json(response);
   }
 );
+
+//拿取變體資料
+router.get(
+  "/variant/:variant_id",
+  async (req: Request, res: Response<ApiResponse>) => {
+    const id = parseInt(req.params.variant_id);
+    if (!id) {
+      const response: ApiErrorResponse = {
+        success: false,
+        error: "找不到該產品",
+      };
+      res.status(401).json(response);
+    }
+    const data = await prisma.productVariant.findUnique({
+      where: { id: id },
+      include: {
+        Product: true,
+      },
+    });
+
+    if (!data) {
+      const response: ApiErrorResponse = {
+        success: false,
+        error: "找不到該產品",
+      };
+      res.status(401).json(response);
+    }
+    const response: ApiResponse = {
+      success: true,
+      data: data,
+    };
+    res.json(response);
+  }
+);
+
+router.get("/checkout", async (req: Request, res: Response) => {
+  try {
+    const baseParams = {
+      // 必填參數
+      MerchantTradeNo: `test${Date.now()}`, // 訂單編號 (唯一值)
+      MerchantTradeDate: new Date().toLocaleString("zh-TW", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      }),
+      TotalAmount: 100, // 交易金額
+      TradeDesc: "測試商品交易", // 交易描述
+      ItemName: "測試商品A#測試商品B", // 商品名稱 (用 # 分隔)
+    };
+
+    const params = {
+      UnionPay: 2, // [需申請] 銀聯卡: 0 (可用, default) | 1 (導至銀聯網) | 2 (不可用)
+    };
+
+    const payment = merchant.createPayment(
+      CreditOneTimePayment,
+      baseParams,
+      params
+    );
+    const htmlRedirectPostForm = await payment.checkout();
+    res.send(htmlRedirectPostForm);
+  } catch (error) {
+    console.error("建立訂單失敗:", error);
+    res.status(500).send("建立訂單時發生錯誤。");
+  }
+});
+
+router.post("/order", (req: Request, res: Response) => {
+  console.log("test");
+  res.send("1|OK");
+});
 
 export default router;
